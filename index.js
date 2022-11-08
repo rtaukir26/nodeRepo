c19a434c54b08d5534bf2097a6b35b3e1c4a02d0
taukir <rtaukir26@gmail.com>
2022-11-08 20:51:33 +0530

    first commit, create http server

---
+++ index.js
@@ -0,0 +1,17 @@
+// importing from another file
+// const app=require("./app")
+
+
+//creating server
+// const http=require("http");
+// http.createServer((req,resp)=>{
+//     // resp.write("hello sending response");
+//     resp.write("<h2>hello sending response</h2>");
+//     resp.end();
+// }).listen(4500);
+
+
+// external package using, coloring
+// const colors=require("colors");
+// console.log("hello welcome".green)
+// console.log("hello welcome".bgGreen)
